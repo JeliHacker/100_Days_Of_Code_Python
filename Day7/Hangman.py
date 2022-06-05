128c3ad01712b2d63a4357150ec3269fe4340242
JeliHacker <jeligooch@gmail.com>
2022-06-05 02:25:56 -0400

    improved hangman

--- Day7/Hangman.py
+++ Day7/Hangman.py
@@ -36,24 +36,28 @@ while not end_of_game:
       print(f"You already guessed '{guess}'!\nYou have not been deducted.")
 
     else:
+
+      
+      
       #Check guessed letter
       for position in range(word_length):
           letter = chosen_word[position]
           # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
           if letter == guess:
-              guessedLetters += letter
               display[position] = letter
   
       #Check if user is wrong.
       if guess not in chosen_word:
+          guessedLetters += guess
           #TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
           lives -= 1
           if lives == 0:
               end_of_game = True
-              print("You lose.")
+              print(f"You lose.\nThe word was {chosen_word}")
 
     #Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
+    print(f"Guessed: {', '.join(guessedLetters)}")
 
     #Check if user has got all letters.
     if "_" not in display:
