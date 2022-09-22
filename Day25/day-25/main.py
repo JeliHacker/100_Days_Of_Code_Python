0f32dccd46a7fe71b48e20022c2d77305fdf428a
JeliHacker <jeligooch@gmail.com>
2022-09-22 22:33:48 +0200

    through lesson 228

---
+++ Day25/day-25/main.py
@@ -0,0 +1,48 @@
+# with open("weather_data.csv") as data_file:
+#     data = data_file.readlines()
+#     print(data)
+
+# import csv
+#
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+#     print(temperatures)
+
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+# print(type(data))
+# print(type(data["temp"]))
+
+# data_dict = data.to_dict()
+# print(data_dict)
+#
+# temp_list = data["temp"].to_list()
+# print(len(temp_list))
+#
+# print(data["temp"].mean())
+# print(data["temp"].max())
+#
+# # Get Data in columns
+# print(data["condition"])
+# print(data.condition)
+
+# Get Data in Row
+# print(data[data.day == "Monday"])
+# print(data[data.temp == data["temp"].max()])
+
+# monday = data[data.day == "Monday"]
+# fahrenheit = monday.temp * (9/5) + 32
+# print(fahrenheit)
+
+# Create a dataframe from scratch
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+data = pandas.DataFrame(data_dict)
+data.to_csv("new_data.csv")
