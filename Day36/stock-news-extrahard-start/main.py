ef8276af7d917496e0fad232ba04c35db64bc56d
JeliHacker <jeligooch@gmail.com>
2023-01-04 23:12:10 -0500

    continue Day36

--- Day36/stock-news-extrahard-start/main.py
+++ Day36/stock-news-extrahard-start/main.py
@@ -4,11 +4,13 @@ COMPANY_NAME = "Tesla Inc"
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
+
 ## STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
 
+
 ## STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number. 
+# Send a separate message with the percentage change and each article's title and description to your phone number.
 
 
 #Optional: Format the SMS message like this: 
