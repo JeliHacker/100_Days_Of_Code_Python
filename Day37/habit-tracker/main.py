0b1efe99fcdf2489f0a3a76e3a649a216c676ba1
JeliHacker <jeligooch@gmail.com>
2023-01-12 22:30:45 -0500

    Day 37 started

---
+++ Day37/habit-tracker/main.py
@@ -0,0 +1,13 @@
+import requests
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": "mytokenmytokenmytokenmytoken",
+    "username": "jeli",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
+
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
