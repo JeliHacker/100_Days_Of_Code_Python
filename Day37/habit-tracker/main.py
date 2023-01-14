c10c30d49dede04e1bf4c8763709536829307d0e
JeliHacker <jeligooch@gmail.com>
2023-01-14 17:12:31 -0500

    Day 37 complete

--- Day37/habit-tracker/main.py
+++ Day37/habit-tracker/main.py
@@ -1,13 +1,64 @@
 import requests
+from datetime import datetime
+
+import config
+
+USERNAME = "jeli"
+TOKEN = config.TOKEN
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
-    "token": "mytokenmytokenmytokenmytoken",
-    "username": "jeli",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
 
 # response = requests.post(url=pixela_endpoint, json=user_params)
 # print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+graphID = "graph1"
+
+
+graph_config = {
+    "id": graphID,
+    "name": "Cycling Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "ajisai"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+pixel_endpoint = f"{graph_endpoint}/{graphID}"
+
+today = datetime.now()
+print(today)
+
+pixel_config = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "20",
+}
+
+# response = requests.post(url=pixel_endpoint, json=pixel_config, headers=headers)
+
+print(today.strftime("%Y%m%d"))
+put_endpoint = f"{pixel_endpoint}/{today.strftime('%Y%m%d')}"
+
+put_config = {
+    "quantity": "10"
+}
+
+# response = requests.put(url=put_endpoint, json=put_config, headers=headers)
+
+
+# print(put_endpoint)
+response = requests.delete(url=put_endpoint, headers=headers)
+print(response.text)
