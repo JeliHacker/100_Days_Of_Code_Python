12987d62022dee82f601d1a1fb83aff7d14a73eb
JeliHacker <jeligooch@gmail.com>
2023-01-19 01:01:02 -0500

    Day 38 really complete, environment variables are my bitch

--- Day38/exercise-tracker/main.py
+++ Day38/exercise-tracker/main.py
@@ -1,10 +1,11 @@
-import config
+# import config
 import requests
 import datetime
 import json
+import os
 
-APP_ID = config.APP_ID
-API_KEY = config.API_KEY
+APP_ID = os.environ.get("APP_ID")
+API_KEY = os.environ.get("API_KEY")
 
 endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
@@ -20,33 +21,45 @@ params = {
 #     "query": input("Enter query here: ")
 # }
 
+print(APP_ID)
 headers = {
     "x-app-id": APP_ID,
     "x-app-key": API_KEY,
 }
 
 response = requests.post(url=endpoint, json=params, headers=headers)
+
 exercise_info = json.loads(response.text)
+
 print(type(exercise_info))
+print("exercise info = ", exercise_info)
 exercise_info_specific = exercise_info["exercises"][0]
 duration = exercise_info["exercises"][0]['duration_min']
 calories = exercise_info_specific["nf_calories"]
 exercise = exercise_info_specific["user_input"]
 
-today = datetime.date.today().strftime()
+today = datetime.date.today().strftime("%Y-%m-%d")
 time = datetime.datetime.now().strftime("%H:%M:%S")
 
 url = 'https://api.sheety.co/9136a8d3a5be37375170b62baa74d35e/100DaysOfCodeDay38MyWorkouts/workouts'
+# url = 'https://api.sheety.co/jeligooch/100DaysOfCodeDay38MyWorkouts/workouts'
 
 json = {
-    "Date": today,
-    "Time": time,
-    "Exercise": exercise,
-    "Duration": duration,
-    "Calories": calories,
+    "workout": {
+        "date": today,
+        "time": time,
+        "exercise": exercise,
+        "duration": duration,
+        "calories": calories,
+    }
 }
 
 print(json)
 
-# response = requests.post(url=url, json=json, params=json)
-# print(response.text)
+sheety_headers = headers
+sheety_headers["Authorization"] = os.environ.get("AUTH_TOKEN")
+
+response = requests.post(url=url, json=json, headers=sheety_headers)
+
+print(response.text)
+print(response.status_code)
