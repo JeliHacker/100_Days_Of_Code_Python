e24226c8a363e8b8d725cde6e701ba78b30d7391
JeliHacker <jeligooch@gmail.com>
2023-01-21 15:01:13 -0500

    Day 39 continued

--- Day39/flight-deals-start/data_manager.py
+++ Day39/flight-deals-start/data_manager.py
@@ -1,3 +1,17 @@
+import requests
+import os
+
+
+def get_rows():
+    print("Here are the rows:")
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+    url = "https://api.sheety.co/9136a8d3a5be37375170b62baa74d35e/day39FlightDeals/prices"
+
+    headers = {
+        "Authorization": os.environ.get("SHEETY_AUTH")
+    }
+
+    get_response = requests.get(url=url, headers=headers)
+    print(get_response.json()["prices"])
--- Day39/flight-deals-start/main.py
+++ Day39/flight-deals-start/main.py
@@ -1,1 +1,6 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from data_manager import DataManager, get_rows
+
+dataManager = DataManager()
+
+get_rows()
